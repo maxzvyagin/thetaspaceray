cf933f614c1cebcef0eb6e228b88d8e48b0f382d
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-30 14:57:00 -0800

    coordination with hyper_resilient

--- thetaspaceray/thetaspaceray.py
+++ thetaspaceray/thetaspaceray.py
@@ -7,6 +7,10 @@ from ray.tune.suggest.skopt import SkOptSearch
 from skopt import Optimizer
 import dill
 
+## hacky solution to run on hyperresilient code
+import sys
+sys.path.append("/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/hyper_resilient_experiments")
+
 def create_pickles(func, args):
     f = open("/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/tmp/thetaspaceray_pickled_func", "wb")
     dill.dump(func, f)
